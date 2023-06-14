da1da7907b54a4dd05a5edcd40d0fc9aab26d832
Gabriel C. Cavalheiro <gabrielccavalheiro@gmail.com>
2023-06-14 08:55:26 -0300

    finalização da home

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,24 +1,30 @@
 import { Layout } from "@/layout";
 import { Flex, Text } from "@chakra-ui/react";
-import { Stack, HStack, VStack, Box } from '@chakra-ui/react';
-import {homeImage} from "public/images";
+import { Stack, HStack, VStack, Box, Card, CardBody, Heading, Divider, CardFooter, ButtonGroup, Button } from '@chakra-ui/react';
+import {ecommerce} from "public/images";
 import { Image } from "@chakra-ui/next-js";
 
 export function Home() {
   return (
     <Layout>
-      <Flex>
-          <Stack direction={['column', 'row']} spacing='24px'>
-          <Box w='180px' h='60px' bg='yellow.200'>
-              <Image src={homeImage} alt={"logoIcon"} h={"60px"} w={"180px"} />
-          </Box>
-          <Box w='40px' h='40px' bg='tomato'>
-            2
-          </Box>
-          <Box w='40px' h='40px' bg='pink.100'>
-            3
-          </Box>
-        </Stack>
+      <Flex direction={'row'} justifyContent={"center"} mt={"25px"} mb={"50px"}>
+        <Card maxW='sm'>
+          <CardBody>
+            <Image
+              src={ecommerce}
+              alt='Green double couch with wooden legs'
+              borderRadius='lg'
+            />
+            <Stack mt='6' spacing='3'>
+              <Heading size='md'>Living room Sofa</Heading>
+              <Text>
+                This sofa is perfect for modern tropical spaces, baroque inspired
+                spaces, earthy toned spaces and for people who love a chic design with a
+                sprinkle of vintage design.
+              </Text>
+            </Stack>
+          </CardBody>
+        </Card>
       </Flex>
     </Layout>
   );
